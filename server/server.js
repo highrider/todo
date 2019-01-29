34b1abcb7a6ac34b01d67fbc04cb8ae9a300d826
highriderr <moremorejoy@gmail.com>
2019-01-29 15:22:26 +0300

    test suite for get todos by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -20,6 +21,23 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+//get /todos/123123123
+app.get('/todos/:id', (req, res) => {// здесь мы делаем гет запрос по адресу /todos/:id, на этот айди будет ссылаться req.params.id
+  var id = req.params.id;  
+  if(!ObjectId.isValid(id)) {//проверка на валидность
+    return res.status(404).send({});//если не прошло, то вернем пустой объект со статусом 404
+  }
+  Todo.findById(id).then((todo) => {//выполним поиск по айдишнику в коллекции, если нет , то опять же 404 и отправим
+    if(!todo){
+      res.status(404).send();
+    }
+      res.send({todo});
+  }).catch((e) => {
+    res.status(404).send();
+  });
+}); 
+
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,13 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {ObjectId} = require('mongodb');
 
 const todos = [{
+  _id: new ObjectId(),
   text: 'First test todo'
 }, {
+  _id: new ObjectId(),
   text: 'Second test todo'
 }];
 
@@ -69,3 +72,32 @@ describe('GET /todos', () => {
       .end(done);
   });
 });
+
+
+describe('GET /todos/:id', () => {//это наш тест кейс с названием
+  it('should be get id properrty', (done) => {//это сам тест, его начало, каждый it это новый тест в тестовой батарее describe
+    //то есть в данном it мы проверяем совпадает ли текст в todos[0].text  с тем, что мы полкчаем
+    request(app)  //это грубо говоря запрос на наш тест
+    .get(`/todos/${todos[0]._id.toHexString()}`)//здесь мы делаем гет запрос по адресу/todos/${todos[0]._id.toHexString()} 
+    .expect(200)
+    .expect((res) => {//здесь мы создаем  тест функцию для того, чтобы проверить  совпадает ли результат с текстом в переменной todos
+        expect(res.body.todo.text).toBe(todos[0].text);//а здесь в тест-функции мы проверяем совпадение фактического текста с тем, что хотим видеть
+      })
+      .end(done);
+    });
+    it('should return 404 if todo not found', (done) => {
+      //здесь нам надо сделать запрос с реальным айди и если его нет вернуть 404
+      let hexId = new ObjectId().toHexString();
+      request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+    });
+    it('should return 404 if it not valid id', (done) => {
+      request(app)
+      .get('/todos/1231')
+      .expect(404)
+      .end(done)
+  });
+});
+
